58356e18ecfb3e3f23eba49b37be9bb430992202
abraru@integriks.ru <abraru@integriks.ru>
2022-12-02 15:08:18 +0300

    Удалил тестовый проект

--- ConsoleApp1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Mindbox.TestTaskV2;
-
-Console.WriteLine("Hello, World!");
-var isRectangular = Figure.IsRectangular(4, 3, 5);
-Console.WriteLine($"{isRectangular}");
